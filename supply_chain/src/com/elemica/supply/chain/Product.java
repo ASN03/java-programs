078876271d079e916430b27bbaac4fa9a8305f15
Nalakath <AmaanSajid.Nalakath@elemica.com>
2025-07-08 15:53:24 +0530

    added files

---
+++ supply_chain/src/com/elemica/supply/chain/Product.java
@@ -0,0 +1,10 @@
+package com.elemica.supply.chain;
+
+public class Product {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am in the Product class");
+	}
+
+}
